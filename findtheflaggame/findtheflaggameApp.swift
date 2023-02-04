0cfe21513535d44a070c88c8058247c62ef4d5c0
DevJ2K <ajavontheo1406@gmail.com>
2023-02-04 01:03:30 +0100

    Initial Commit

---
+++ findtheflaggame/findtheflaggameApp.swift
@@ -0,0 +1,17 @@
+//
+//  findtheflaggameApp.swift
+//  findtheflaggame
+//
+//  Created by Théo Ajavon on 04/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct findtheflaggameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
